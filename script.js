1b4add5eba21fdad7110bfef92fda0c91d15be22
Jared Varnum <jvarnum3@gmail.com>
2020-05-23 21:10:19 -0500

    starting to lay out the logic of the password generator, adding in an area for the program to understand what values will make up the password, so that the value can then later be used to interact with the array and pull out our random values

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var password = generatePassword();
+var passwordCharLowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var passwordCharUpperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var specialCharacters = ["!@#$%&*+?^"];
+var numbers = ["1234567890"];
+//Reminder - please make sure to use parse int on the numbers above because they are in a string
+ 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var passwordCharLowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-  var passwordCharUpperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-  var specialCharacters = ["!@#$%&*+?^"];
-  var numbers = ["1234567890"];
-
+  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  if user selects lowercase 
+
+  if user selects UPPER
+
+  if user selects number
+
+  if user selects special specialCharacters
+
+  else "Error: No Password Generated.  Please select at least one (1) character option!"
+}
