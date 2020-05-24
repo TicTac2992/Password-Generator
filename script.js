6151c70af4afa95891f50c80fadadcd876cb0514
Jared Varnum <jvarnum3@gmail.com>
2020-05-23 20:39:15 -0500

    establishing my variables, showing array values that generator will pull from

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var passwordCharLowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+  var passwordCharUpperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var specialCharacters = ["!@#$%&*+?^"];
+  var numbers = ["1234567890"];
 
   passwordText.value = password;
 
