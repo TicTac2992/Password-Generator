74f7107e784e80bb1f5de43aca66c85a51f20d58
Jared Varnum <jvarnum3@gmail.com>
2020-05-23 23:14:40 -0500

    pulling in external resources to make undefined produce a result, appears to be functioning properly now.

--- script.js
+++ script.js
@@ -1,91 +1,107 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
 
+  passwordText.value = password;
 }
 
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var passwordCharLowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var passwordCharUpperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var specialCharacters = ["!@#$%&*+?^()_"];
-var numbers = ["1234567890"];
-var passwordLength = "";
+var generateBtn = document.querySelector("#generate");
+var generatedPassword = document.querySelector("#password");
+
+//Password type variables
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var specialCharacters = "!@#$%&*+?^()_";
+var numbers = "1234567890";
+var length = "";
 var chosenCharacterTypes = [];
-//Reminder - please make sure to use parse int on the numbers above because they are in a string
+
 
 function generatePassword() {
   event.preventDefault();
   let passwordLength = parseInt(
-    prompt("How many characters will your password have? Choose between 8 and 128."));
-
-  console.log("Password length: " + passwordLength);
-
-let confirmUpperCaseLetters = confirm("Do you want to include Upper Case letters in your password?");
-let confirmLowerCaseLetters = confirm("Do you want to include lower case letters in your password?");
-let confirmNumbers = confirm("Do you want to include numbers in your password?");
-let confirmSpecialCharacters = confirm("Last question...I promise. Do you want to include special characters in your password?");
-
-// Add event listener to generate button
-if (confirmUpperCaseLetters) {
-  chosenCharacterTypes.push(passwordCharUpperCase);
-  console.log("Uppercase: Yes");
-} else {
-  console.log("Uppercase: No");
-}
-
-if (confirmLowerCaseLetters) {
-  chosenCharacterTypes.push(passwordCharLowerCase);
-  console.log("Lowercase: Yes");
-} else {
-  console.log("Lowercase: No");
-}
-
-if (confirmNumbers) {
-  chosenCharacterTypes.push(numbers);
-  console.log("Numbers: Yes");
-} else {
-  console.log("Numbers: No");
-}
-
-if (confirmSpecialCharacters) {
-  chosenCharacterTypes.push(specialCharacters);
-  console.log("Special characters: Yes");
-} else {
-  console.log("Special characters: No");
-}
-}
-
-let newPassword = "";
-
-//Compose password by randomly assigning values from combination of selected character types
-for (let i = 0; i < passwordLength; i++) {
-  var selectedCharacterIndex = Math.floor(Math.random() * chosenCharacterTypes.length
+    prompt(
+      "How many characters would you like your password to be? Choose between 8 and 128."
+    )
   );
 
-  var characterString = chosenCharacterTypes[selectedCharacterIndex];
+  console.log("Password length: " + passwordLength); //console.log desired password length
 
-  if (typeof characterString === "undefined") {
-    console.log("No character type selected.");
-    wrongCriteriaEntered();
+  //If entered input is not valid, display prompt again
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(
+      prompt("Please choose a number between 8 and 128.")
+    );
+    console.log("Password length " + passwordLength);
+  }
+  //Prompt options for password character types
+  let confirmUpperCaseLetters = confirm("Do you want uppercase letters in your password?");
+  let confirmLowerCaseLetters = confirm("Do you want lowercase letters in your password?");
+  let confirmNumbers = confirm("Do you want numbers in your password?")
+  let confirmSpecialCharacters = confirm("Do you want to include any special characters?");
 
+  //Store chosen password types
+  if (confirmUpperCaseLetters) {
+    chosenCharacterTypes.push(uppercase);
+    console.log("Uppercase: Yes");
   } else {
-    newPassword +=
-      characterString[Math.floor(Math.random() * characterString.length)];
-
-    console.log(newPassword);
+    console.log("Uppercase: No");
   }
 
-  function wrongCriteriaEntered() {
-    document.getElementById("password").innerHTML =
-      "Oops...looks like you did not select any character type. Please try again and choose at least one.";
-      return wrongCriteriaEntered();
+  if (confirmLowerCaseLetters) {
+    chosenCharacterTypes.push(lowercase);
+    console.log("Lowercase: Yes");
+  } else {
+    console.log("Lowercase: No");
   }
+
+  if (confirmNumbers) {
+    chosenCharacterTypes.push(numbers);
+    console.log("Numbers: Yes");
+  } else {
+    console.log("Numbers: No");
+  }
+
+  if (confirmSpecialCharacters) {
+    chosenCharacterTypes.push(specialCharacters);
+    console.log("Special characters: Yes");
+  } else {
+    console.log("Special characters: No");
+  }
+
+  //this will allow whatever indexes are selected to populate into the "new password" field
+  let newPassword = "";
+
+  //Compose password by randomly assigning values from combination of selected character types
+  for (let i = 0; i < passwordLength; i++) {
+    var selectedCharacterIndex = Math.floor(
+      Math.random() * chosenCharacterTypes.length
+    );
+
+    var characterString = chosenCharacterTypes[selectedCharacterIndex];
+
+    if (typeof characterString === "undefined") {
+      console.log("No character type selected.");
+      wrongCriteriaEntered();
+
+    } else {
+      newPassword +=
+        characterString[Math.floor(Math.random() * characterString.length)];
+
+      console.log(newPassword);
+    }
+
+    function wrongCriteriaEntered() {
+      document.getElementById("password").innerHTML =
+        "Error: Please select at least one (1) character type for your password to randomly generate!";
+        return wrongCriteriaEntered();
+    }
+  }
+  return newPassword;
 }
